f72fe2cf22eee15cfb29dc3138ac20db31d521ef
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-07 17:54:30 +0200

    Add Ueberauth Github config

--- config/config.exs
+++ config/config.exs
@@ -31,6 +31,9 @@ config :ueberauth, Ueberauth,
     github: { Ueberauth.Strategy.Github, [] }
   ]
 
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: System.get_env("GITHUB_CLIENT_ID"),
+  client_secret: System.get_env("GITHUB_CLIENT_SECRET")
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
