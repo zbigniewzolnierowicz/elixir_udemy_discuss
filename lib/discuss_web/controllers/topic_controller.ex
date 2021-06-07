35a05c99a2185c5f6306e6f7ec8933f955fb5d02
Zbigniew Zolnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-07 16:07:49 +0200

    Run a formatter

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -6,33 +6,38 @@ defmodule DiscussWeb.TopicController do
 
   def new_form(conn, params) do
     changeset = Topic.changeset(%Topic{}, params)
-    render conn, "index.html", changeset: changeset
+    render(conn, "index.html", changeset: changeset)
   end
 
   def create(conn, %{"topic" => update_data}) do
     changeset = Topic.changeset(%Topic{}, update_data)
+
     case Repo.insert(changeset) do
       {:ok, _post} ->
         conn
         |> put_flash(:info, "You did it! You successfully made this topic!")
         |> redirect(to: Routes.topic_path(conn, :index))
+
       {:error, changeset} ->
-        render conn, "index.html", changeset: changeset
+        render(conn, "index.html", changeset: changeset)
     end
-    text conn, "good job"
+
+    text(conn, "good job")
   end
 
   def index(conn, _params) do
-    topics = Repo.all(
-      from t in Topic,
-        select: %Topic{id: t.id, title: t.title},
-        order_by: [t.id]
-    )
+    topics =
+      Repo.all(
+        from t in Topic,
+          select: %Topic{id: t.id, title: t.title},
+          order_by: [t.id]
+      )
+
     render(conn, "topics.html", topics: topics)
   end
 
   def delete(conn, _params) do
-    text conn, "Unimplemented"
+    text(conn, "Unimplemented")
   end
 
   def update(conn, %{"id" => id, "topic" => params}) do
@@ -44,14 +49,15 @@ defmodule DiscussWeb.TopicController do
         conn
         |> put_flash(:info, "You successfully edited the post with ID #{post.id}")
         |> redirect(to: Routes.topic_path(conn, :index))
+
       {:error, changeset} ->
-        render conn, "edit.html", topic: topic, changeset: changeset
+        render(conn, "edit.html", topic: topic, changeset: changeset)
     end
   end
 
   def edit_form(conn, %{"id" => id}) do
     topic = Repo.get(Topic, id)
     changeset = Topic.changeset(topic, %{})
-    render conn, "edit.html", topic: topic, changeset: changeset
+    render(conn, "edit.html", topic: topic, changeset: changeset)
   end
 end
