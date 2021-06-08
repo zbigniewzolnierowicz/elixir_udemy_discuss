6abefd76aca72c2051ceef9cc9d97cb5f6595b90
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-08 17:39:24 +0200

    Add user controls

--- lib/discuss_web/controllers/auth_controller.ex
+++ lib/discuss_web/controllers/auth_controller.ex
@@ -1,10 +1,37 @@
 defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
+  alias Discuss.User
+  alias Discuss.Repo
 
-  def callback(conn, params) do
-    IO.inspect(conn.assigns)
-    IO.inspect(params)
-    text conn, "Unimplemented"
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
+    user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+    changeset = User.changeset(%User{}, user_params)
+
+    signin(conn, changeset)
+  end
+
+  defp signin(conn, changeset) do
+    case insert_or_update_user(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "User #{user.email} was signed in.")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, _changeset} ->
+        conn
+        |> put_flash(:error, "User could not be signed in.")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
+    text conn, "unimplemeneted"
+  end
+
+  defp insert_or_update_user(changeset) do
+    case Repo.get_by(User, email: changeset.changes.email) do
+      nil ->
+        Repo.insert(changeset)
+      user ->
+        {:ok, user}
+    end
   end
 end
